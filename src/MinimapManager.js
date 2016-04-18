f6ab71ae5378ded6871b3e45d85598d03f2359be
BuildingJarl <ivanbacher@socs-air-2.ict.ad.dit.ie>
2016-04-18 10:52:25 +0100

    refactor v2

--- src/MinimapManager.js
+++ src/MinimapManager.js
@@ -10,6 +10,7 @@ export default class MinimapManager {
 		var height = container.clientHeight
 		
 		this.selectedNode =  null;
+		this.container = container;
 
 		this.svg = d3.select(container).append("svg")
 			.attr("width", width)
@@ -19,55 +20,8 @@ export default class MinimapManager {
 
 		EventDispatcher.addEventListener('editor_change', (event) => {
 
-			parse(event.payload).then( (dom) => {
-				this.selectedNode = null;
-				this.visGroup.selectAll("*").remove();
-				
-				if(dom.length >= 1) {
-					var root = { type: 'root', name: 'html', children: dom };
-
-					var level = heightHeighy(root);
-					
-					if(level > 0) {
-					
-						//this has to be width + width/maxNestingLevel
-						var widthNew = (visContainer.clientWidth + visContainer.clientWidth/level);
-						var partition = d3.layout.partition()
-					  		.size([height,widthNew])
-					  		.value(function(d) { return 1; })
-					  		.children(function(d){
-					  				if(d.children) {
-					  					return d.children.filter(function(o){
-						  					if(o.type === 'tag') {
-						  						return true;
-						  					} else {
-						  						return false;
-						  					}
-						  				});
-					  				}
-					  		})
-					  		.sort(null);
-
-						var nodes = partition.nodes(root);
-						this.visGroup.attr("transform", "translate("+ -(nodes[0].dy)+",0)");
-
-						this.visGroup.selectAll(".node")
-				      		.data(nodes)
-				    		.enter().append("rect")
-				      		.attr("class", "node")
-					      	.attr("x", function(d) { return d.y; })
-					     	.attr("y", function(d) { return d.x; })
-					      	.attr("width", function(d) { return d.dy; })
-					      	.attr("height", function(d) { return d.dx; })
-					      	.style("fill", function(d) {
-					      		if(d.type === 'tag') {
-					      			return 'blue';
-					      		} else {
-					      			return 'yellow';
-					      		}
-					      	});
-			    	}
-				} 
+			this.parse(event.payload).then( (dom) => {
+				this.update(dom);
 				EventDispatcher.dispatchEvent( {type:'editor_change_async_end'} );
 			});
 		});
@@ -83,6 +37,7 @@ export default class MinimapManager {
 					.duration(250)
 					.style("fill","blue");
 
+				this.selectedNode = null;
 				selectedNode = null;
 			}	
 
@@ -103,12 +58,87 @@ export default class MinimapManager {
 			});
 
 			if(selectedNode) {
+				this.selectedNode = selectedNode;
 				d3.select(selectedNode)
 					.transition()
 					.duration(250)
 					.style("fill","green");;
 			}
 		});
+	}
+
+	parse(payload) {
+		var promise = new Promise( function(resolve, reject) {
+			var handler = new htmlparser.DomHandler( function (error, dom) {
+
+				if (error) {
+					console.log(error);
+					reject(err);
+				}
+				else
+					resolve(dom);
+			}, {withStartIndices:true, withEndIndices:true});
+
+			var parser = new htmlparser.Parser(handler, {recognizeSelfClosing: true, decodeEntities: true});
+
+			parser.write(payload);
+			parser.done();
+		});
+
+		return promise;
+	}
+
+	update(dom) {
+
+		this.selectedNode = null;
+		this.visGroup.selectAll("*").remove();
+		
+		if(dom.length >= 1) {
+			var root = { type: 'root', name: 'html', children: dom };
+
+			var level = heightHeighy(root);
+			
+			if(level > 0) {
+			
+				//this has to be width + width/maxNestingLevel
+				var height = this.container.clientHeight;
+				var widthNew = (this.container.clientWidth + this.container.clientWidth/level);
+				var partition = d3.layout.partition()
+			  		.size([height,widthNew])
+			  		.value(function(d) { return 1; })
+			  		.children(function(d){
+			  				if(d.children) {
+			  					return d.children.filter(function(o){
+				  					if(o.type === 'tag') {
+				  						return true;
+				  					} else {
+				  						return false;
+				  					}
+				  				});
+			  				}
+			  		})
+			  		.sort(null);
+
+				var nodes = partition.nodes(root);
+				this.visGroup.attr("transform", "translate("+ -(nodes[0].dy)+",0)");
+
+				this.visGroup.selectAll(".node")
+		      		.data(nodes)
+		    		.enter().append("rect")
+		      		.attr("class", "node")
+			      	.attr("x", function(d) { return d.y; })
+			     	.attr("y", function(d) { return d.x; })
+			      	.attr("width", function(d) { return d.dy; })
+			      	.attr("height", function(d) { return d.dx; })
+			      	.style("fill", function(d) {
+			      		if(d.type === 'tag') {
+			      			return 'blue';
+			      		} else {
+			      			return 'yellow';
+			      		}
+			      	});
+	    	}
+		} 
 	}
 }
 
@@ -128,28 +158,4 @@ function heightHeighy(el) {
        }
     }
     return max + 1;
-}
-
-// ----------------------------------------------------
-//     Parser Wrapper
-function parse(payload) {
-	
-	var promise = new Promise( function(resolve, reject) {
-		var handler = new htmlparser.DomHandler( function (error, dom) {
-			//console.log('parsing ...');
-
-			if (error)
-				console.log(error);
-				//reject(err);
-			else
-				resolve(dom);
-		}, {withStartIndices:true, withEndIndices:true});
-
-		var parser = new htmlparser.Parser(handler, {recognizeSelfClosing: true, decodeEntities: true});
-
-		parser.write(payload);
-		parser.done();
-	});
-
-	return promise;
 }
