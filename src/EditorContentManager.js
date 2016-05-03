fe854ff8f414b5021b2f74e6eabf7a88cfa0522a
BuildingJarl <ivanbacher@socs-air-2.ict.ad.dit.ie>
2016-05-03 12:27:32 +0100

    save

---
+++ src/EditorContentManager.js
@@ -0,0 +1,16 @@
+class EditorContentManager {
+	
+	constructor() {
+		this.content = [];
+	}
+
+	addNewContent(newContent) {
+
+		var split = newContent.split('\n');
+
+	}
+
+	
+}
+
+export default new EditorContentManager();
